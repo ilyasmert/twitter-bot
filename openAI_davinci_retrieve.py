4795db50582a176509f7a63b1d5531a697a42364
ilyas mert unur <127152720+ilyasmert@users.noreply.github.com>
2023-04-11 18:40:44 +0300

    Delete openAI_davinci_retrieve.py

--- openAI_davinci_retrieve.py
+++
@@ -1,5 +0,0 @@
-import os
-import openai
-openai.organization = "org-VdJGu4VbYFMXHJoqY3ZUFBkE"
-openai.api_key = "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"
-openai.Model.retrieve("text-davinci-003")
